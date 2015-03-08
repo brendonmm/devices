11a4b9c9f1f5d40845252ebfb200ab5ba780aa3e
Brendo Marinho <brendonmm@outlook.com>
2015-03-08 03:50:36 -0300

    Adicionado Font-Awsome, novos Dispositivos e alterados nomes de estilos.

--- index.php
+++ index.php
@@ -8,19 +8,48 @@
     <script src="js/jquery.min.js" type="text/javascript"></script>
     <script src="js/jquery.tablesorter.min.js" type="text/javascript"></script>
     
-	<link href="css/style.css" type="text/css" rel="stylesheet" />
-	<link href="css/theme.default.css" type="text/css" rel="stylesheet" />
-	<link href="css/theme.blue.css" type="text/css" rel="stylesheet" />
-	<link href="css/theme.dark.css" type="text/css" rel="stylesheet" />
-	<link href="css/theme.grey.css" type="text/css" rel="stylesheet" />
-	<link href="css/theme.metro-dark.css" type="text/css" rel="stylesheet" />
-	<link href="css/filter.formatter.css" type="text/css" rel="stylesheet" />
+	<link href="css/tablesorter.theme.default.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/tablesorter.theme.blue.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/tablesorter.theme.dark.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/tablesorter.theme.grey.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/tablesorter.theme.metro-dark.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/tablesorter.filter.formatter.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 <body>
     <div id="page-wrap">
         <h1>RWD Devices</h1>
         <h2>Principais dispositivos</h2>
         <p>Aqui você encontra os principais dispositivos do mercado e informações para desenvolver seu app e site melhor.</p>
+        <h2>Como funciona</h2>
+        <p>As dimensões em pixel são a contagem de pixel no hardware físico.</p>
+        <ul class="lista">
+            <li>Smartphones estão listados no modo Retrato (portrait), por isso a largura sempre é menor que a altura (width x height).</li>
+            <li>Já nos tablets e PCs, estão listados no modo Paisagem (landscape), por se tratarem de dispositivos utilizados nesse modo por padrão, sendo assim, a largura sempre será maior que a altura.</li>
+        </ul>
+        <p>CSS Pixel Ratio é a taxa de pixels entre o pixel do hardware e o CSS Pixel, definido pelas especificações do CSS 2.1.</p>
+        <ul class="lista" lang="en">
+            <li><strong>ldpi</strong> (<em>low</em>) ~120dpi - 0.75</li>
+            <li><strong>mdpi</strong> (<em>medium</em>) ~160dpi - 1</li>
+            <li><strong>hdpi</strong> (<em>high</em>) ~240dpi - 1.5</li>
+            <li><strong>xhdpi</strong> (<em>extra-high</em>) ~320dpi - 2</li>
+            <li><strong>xxhdpi</strong> (<em>extra-extra-high</em>) ~480dpi - 3</li>
+            <li><strong>xxxhdpi</strong> (<em>extra-extra-extra-high</em>) ~640dpi - 4</li>
+        </ul>
+        <p>As telas de alta densidade (<em lang="en">Hight Density</em>), costumam ter mais de 200dpis, então os valores de pixels devem ser divididos para a escala de referência correta ao pixel utilizado.</p>
+        <blockquote cite="http://www.w3.org/TR/CSS2/syndata.html#length-units" >
+            <q>The reference pixel is the visual angle of one pixel on a device with a pixel density of 96dpi and a distance from the reader of an arm's length. For a nominal arm's length of 28 inches, the visual angle is therefore about 0.0213 degrees. For reading at arm's length, 1px thus corresponds to about 0.26 mm (1/96 inch).</q>
+            <cite>http://www.w3.org/TR/CSS2/syndata.html#length-units</cite>
+        </blockquote>
+        <p>As dimensões do dispositivo são as dimensões de CSS Pixel em escala de 100%. Podemos indicar isso ao dispositivo por meio da <em>viewport</em> meta tag:</p>
+        <pre class="javascript">&lt;meta&nbsp;name=<span class="string">"viewport"</span>&nbsp;content=<span class="string">"width=device-width"</span> /&gt;<br></pre>
+        <p>O CSS Pixel Width pode ser calculado dividindo a largura do dispositivo (valor de pixels), por sua taxa de pixels (CSS Pixel Ratio), e arredondando o valor para inteiro.</p>
+        <div class="alert">
+            <span>Legenda:</span><span class="span-novo">ADICIONADO RECENTEMENTE</span> <span class="span-descontinuado">DESCONTINUADO</span> <span class="span-obsoleto">OBSOLETO</span>
+        </div>
+    </div>
+    <div class="container-table">
         <table id="dispositivos" border='1' class="table" lang="en">
             <thead>
                 <tr>
@@ -39,9 +68,6 @@
             </thead>
             <tbody></tbody>
         </table>
-        <div class="alert">
-            <span>Legenda:</span><span class="span-novo">ADICIONADO RECENTEMENTE</span> <span class="span-descontinuado">DESCONTINUADO</span>
-        </div>
     </div>
     <script type="text/javascript" src="js/jquery.tablesorter.widgets.min.js"></script>
     <script type="text/javascript" src="js/widgets/widget-reorder.js"></script>
@@ -103,8 +129,10 @@
                 for (b = 0; b < dispositivo.length; b++) {
                     if (dispositivo[b].novo === 'yes') {
                         buffer += "<tr class=\"novo\">";
-                    } else if (dispositivo[b].novo === 'old') {
+                    } else if (dispositivo[b].novo === 'des') {
                         buffer += "<tr class=\"descontinuado\">";
+                    } else if (dispositivo[b].novo === 'obs') {
+                        buffer += "<tr class=\"obsoleto\">";
                     } else {
                         buffer += "<tr>";
                     }
@@ -196,6 +224,32 @@
             xml = xmlLoader("meus_devices.xml");
             xmlParserDispositivosSimplificado(xml);
         });
+
+        /* GOOGLE FONTS PT Sans */
+        (function() {
+            var link_element = document.createElement("link"),
+                s = document.getElementsByTagName("script")[0];
+            if (window.location.protocol !== "http:" && window.location.protocol !== "https:") {
+                link_element.href = "http:";
+            }
+            link_element.href += "//fonts.googleapis.com/css?family=PT+Sans:400italic,400,700italic,700";
+            link_element.rel = "stylesheet";
+            link_element.type = "text/css";
+            s.parentNode.insertBefore(link_element, s);
+        })();
+
+        /* GOOGLE FONTS Open Sans */
+        (function() {
+            var link_element = document.createElement("link"),
+                s = document.getElementsByTagName("script")[0];
+            if (window.location.protocol !== "http:" && window.location.protocol !== "https:") {
+                link_element.href = "http:";
+            }
+            link_element.href += "//fonts.googleapis.com/css?family=Open+Sans:300italic,300,400italic,400,600italic,600,700italic,700,800italic,800";
+            link_element.rel = "stylesheet";
+            link_element.type = "text/css";
+            s.parentNode.insertBefore(link_element, s);
+        })();
     </script>
 </body>
 </html>
